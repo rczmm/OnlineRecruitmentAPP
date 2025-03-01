a30c84e6ee2bfe3602d8c3bbd078412259cad64b
rcz <3179623450@qq.com>
2025-03-01 16:07:58 +0800

    Refactor job listing logic into reusable components
    Renamed `JobList` to `JobListContainer` and extracted core job list features into a new `JobListView` component. This improves code modularity, reusability, and separates responsibilities. Updated HomeScreenContent with these changes and included generation of sample job data for testing.

--- lib/screens/HomeScreenContent.dart
+++ lib/screens/HomeScreenContent.dart
@@ -6,13 +6,139 @@ class HomeScreenContent extends StatelessWidget {
   const HomeScreenContent({super.key});
 
   List<Job> _loadMoreJobs() {
-    // 这里可以调用API获取更多职位数据
-    // 暂时返回空列表，实际数据会由JobList中的_generateSampleJobs生成
-    return [];
+    // 使用示例数据
+    return _generateSampleJobs();
   }
 
   @override
   Widget build(BuildContext context) {
-    return JobList(onLoadMore: _loadMoreJobs);
+    return JobListContainer(onLoadMore: _loadMoreJobs);
   }
+
+  List<Job> _generateSampleJobs() {
+    final titles = [
+      '高级Java开发工程师',
+      'Flutter移动开发工程师',
+      'Python数据分析师',
+      '前端开发工程师',
+      'DevOps工程师',
+      '产品经理',
+      'UI设计师',
+      '算法工程师',
+      '测试工程师',
+      '运维工程师'
+    ];
+
+    final companies = [
+      {'name': '字节跳动', 'size': '10000人以上'},
+      {'name': '腾讯科技', 'size': '10000人以上'},
+      {'name': '阿里巴巴', 'size': '10000人以上'},
+      {'name': '美团点评', 'size': '10000人以上'},
+      {'name': '快手科技', 'size': '5000-10000人'},
+      {'name': '小米科技', 'size': '5000-10000人'},
+      {'name': '京东集团', 'size': '10000人以上'},
+      {'name': '网易', 'size': '5000-10000人'},
+      {'name': '百度', 'size': '10000人以上'},
+      {'name': '滴滴出行', 'size': '5000-10000人'}
+    ];
+
+    final salaries = [
+      '15k-25k',
+      '20k-35k',
+      '25k-45k',
+      '30k-50k',
+      '35k-60k',
+      '40k-70k',
+      '45k-80k',
+      '50k-90k',
+      '60k-100k',
+      '面议'
+    ];
+
+    final locations = [
+      '北京市朝阳区',
+      '上海市浦东新区',
+      '深圳市南山区',
+      '广州市天河区',
+      '杭州市西湖区',
+      '成都市高新区',
+      '武汉市洪山区',
+      '南京市江宁区',
+      '西安市雁塔区',
+      '苏州市工业园区'
+    ];
+
+    final allTags = {
+      '开发': [
+        'Java',
+        'Spring Boot',
+        'MySQL',
+        'Redis',
+        'MQ',
+        'Flutter',
+        'Python',
+        'Django',
+        'React',
+        'Vue.js',
+        'Node.js',
+        'TypeScript',
+        'Docker',
+        'K8s',
+        'AWS'
+      ],
+      '设计': ['UI设计', 'UE设计', 'Figma', 'Sketch', 'PhotoShop', '原型设计', '交互设计'],
+      '产品': ['需求分析', '产品规划', '用户研究', '数据分析', 'Axure', '项目管理'],
+      '算法': ['机器学习', '深度学习', 'NLP', '计算机视觉', 'PyTorch', 'TensorFlow']
+    };
+
+    final hrNames = [
+      '王女士',
+      '李先生',
+      '张女士',
+      '刘先生',
+      '陈女士',
+      '赵先生',
+      '孙女士',
+      '周先生',
+      '吴女士',
+      '郑先生'
+    ];
+
+    return List.generate(10, (index) {
+      final company = companies[index];
+      final title = titles[index];
+      String category = '';
+      if (title.contains('开发'))
+        category = '开发';
+      else if (title.contains('设计'))
+        category = '设计';
+      else if (title.contains('产品'))
+        category = '产品';
+      else if (title.contains('算法'))
+        category = '算法';
+      else
+        category = '开发';
+
+      final tagPool = allTags[category] ?? allTags['开发']!;
+      final selectedTags = (tagPool.toList()..shuffle()).take(3).toList();
+
+      return Job(
+          id: 'job_${index + 1}',
+          title: title,
+          salary: salaries[index],
+          company: company['name']!,
+          companySize: company['size']!,
+          tags: selectedTags,
+          hrName: hrNames[index],
+          location: locations[index],
+          workExperience: '3-5年',
+          education: '本科及以上',
+          benefits: ['五险一金', '年终奖', '带薪年假', '加班补助'],
+          description: '负责公司核心业务系统的开发和维护工作',
+          requirements: ['本科及以上学历', '3年以上相关工作经验', '良好的团队协作能力'],
+          status: '未投递',
+          date: DateTime.now().toString().substring(0, 10));
+    });
+  }
+
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,16 +6,17 @@ import '../models/job.dart';
 import '../widgets/job_card.dart';
 import 'search_screen.dart';
 
-class JobList extends StatefulWidget {
+class JobListContainer extends StatefulWidget {
   final List<Job> Function() onLoadMore;
 
-  const JobList({super.key, required this.onLoadMore});
+  const JobListContainer({super.key, required this.onLoadMore});
 
   @override
-  State<JobList> createState() => _JobListState();
+  State<JobListContainer> createState() => _JobListContainerState();
 }
 
-class _JobListState extends State<JobList> with TickerProviderStateMixin {
+class _JobListContainerState extends State<JobListContainer>
+    with TickerProviderStateMixin {
   final TextEditingController _searchController = TextEditingController();
   List<String> _keywords = [
     '技术开发',
@@ -30,57 +31,30 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
   String _errorMessage = '';
   String _currentMainTab = '';
   String _currentSubTab = '推荐';
-  late TabController _mainTabController = TabController(length: _keywords.length, vsync: this);
+  late TabController _mainTabController =
+      TabController(length: _keywords.length, vsync: this);
   late TabController _subTabController = TabController(length: 3, vsync: this);
-  List<JobList> _jobLists = [];
-  final ScrollController _scrollController = ScrollController();
-  List<Job> _jobs = [];
-  bool _isLoading = false;
-  
+  List<JobListView> _jobListViews = [];
+
   @override
   void initState() {
     super.initState();
     _fetchKeywords();
-    // 初始化三个 JobList，对应推荐、附近、最新三个标签页
-    _jobLists = List.generate(3, (index) => JobList(
-      key: GlobalKey<_JobListState>(),
-      onLoadMore: _refreshJobList,
-    ));
-    _loadJobs();
-    _scrollController.addListener(_onScroll);
-  }
-  
-  void _loadJobs() {
-    if (!_isLoading) {
-      setState(() {
-        _isLoading = true;
-      });
-      _jobs.addAll(widget.onLoadMore());
-      setState(() {
-        _isLoading = false;
-      });
-    }
+    // 初始化三个 JobListView，对应推荐、附近、最新三个标签页
+    _jobListViews = List.generate(
+        3,
+        (index) => JobListView(
+              key: GlobalKey<_JobListViewState>(
+                  debugLabel: 'JobListView_$index'),
+              onLoadMore: widget.onLoadMore,
+            ));
   }
 
-  void refreshJobList() {
-    setState(() {
-      _jobs.clear();
-    });
-    _loadJobs();
-  }
-
-  void _onScroll() {
-    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-      _loadJobs();
-    }
-  }
-  
   Future<void> _fetchKeywords() async {
     setState(() {
-      _isLoading = true;
       _errorMessage = '';
     });
-  try {
+    try {
       final response = await dio.get("getKeywords");
       // 判断返回数据是否正确
       if (response.statusCode == 200 &&
@@ -114,17 +88,9 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
       if (_keywords.isNotEmpty) {
         _currentMainTab = _keywords[0];
       }
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
     }
   }
-  // 刷新职位列表数据
-  List<Job> _refreshJobList() {
-    // TODO: 这里将来需要根据_currentMainTab和_currentSubTab调用后端接口获取数据
-    return _generateSampleJobs();
-  }
+
   void _onSearch(String query) {
     Navigator.push(
       context,
@@ -136,12 +102,12 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    _scrollController.dispose();
     _searchController.dispose();
     _mainTabController.dispose();
     _subTabController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -191,10 +157,12 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
                 unselectedLabelColor: Colors.grey,
                 indicatorColor: Colors.blue,
                 indicatorWeight: 3,
-                tabs: _keywords.map((keyword) => Tab(
-                  text: keyword,
-                  height: 44,
-                )).toList(),
+                tabs: _keywords
+                    .map((keyword) => Tab(
+                          text: keyword,
+                          height: 44,
+                        ))
+                    .toList(),
                 onTap: (index) {
                   setState(() {
                     _currentMainTab = _keywords[index];
@@ -202,8 +170,11 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
                   _subTabController.index = 0;
                   _currentSubTab = '推荐';
                   // 刷新当前标签页的职位列表
-                  if (_subTabController.index >= 0 && _subTabController.index < _jobLists.length) {
-                    final state = (_jobLists[_subTabController.index].key as GlobalKey<_JobListState>).currentState;
+                  if (_subTabController.index >= 0 &&
+                      _subTabController.index < _jobListViews.length) {
+                    final state = (_jobListViews[_subTabController.index].key
+                            as GlobalKey<_JobListViewState>)
+                        .currentState;
                     state?.refreshJobList();
                   }
                 },
@@ -231,106 +202,124 @@ class _JobListState extends State<JobList> with TickerProviderStateMixin {
         Expanded(
           child: TabBarView(
             controller: _subTabController,
-            children: _jobLists.map((jobList) => jobList).toList(),
+            children: _jobListViews,
           ),
         ),
       ],
     );
   }
-  // 生成示例数据
-  List<Job> _generateSampleJobs() {
-    final titles = [
-      '高级Java开发工程师',
-      'Flutter移动开发工程师',
-      'Python数据分析师',
-      '前端开发工程师',
-      'DevOps工程师',
-      '产品经理',
-      'UI设计师',
-      '算法工程师',
-      '测试工程师',
-      '运维工程师'
-    ];
+}
 
-    final companies = [
-      {'name': '字节跳动', 'size': '10000人以上'},
-      {'name': '腾讯科技', 'size': '10000人以上'},
-      {'name': '阿里巴巴', 'size': '10000人以上'},
-      {'name': '美团点评', 'size': '10000人以上'},
-      {'name': '快手科技', 'size': '5000-10000人'},
-      {'name': '小米科技', 'size': '5000-10000人'},
-      {'name': '京东集团', 'size': '10000人以上'},
-      {'name': '网易', 'size': '5000-10000人'},
-      {'name': '百度', 'size': '10000人以上'},
-      {'name': '滴滴出行', 'size': '5000-10000人'}
-    ];
+class JobListView extends StatefulWidget {
+  final List<Job> Function() onLoadMore;
 
-    final salaries = [
-      '15k-25k',
-      '20k-35k',
-      '25k-45k',
-      '30k-50k',
-      '35k-60k',
-      '40k-70k',
-      '45k-80k',
-      '50k-90k',
-      '60k-100k',
-      '面议'
-    ];
+  const JobListView({super.key, required this.onLoadMore});
 
-    final locations = [
-      '北京市朝阳区',
-      '上海市浦东新区',
-      '深圳市南山区',
-      '广州市天河区',
-      '杭州市西湖区',
-      '成都市高新区',
-      '武汉市洪山区',
-      '南京市江宁区',
-      '西安市雁塔区',
-      '苏州市工业园区'
-    ];
+  @override
+  State<JobListView> createState() => _JobListViewState();
+}
 
-    final allTags = {
-      '开发': ['Java', 'Spring Boot', 'MySQL', 'Redis', 'MQ', 'Flutter', 'Python', 'Django', 
-              'React', 'Vue.js', 'Node.js', 'TypeScript', 'Docker', 'K8s', 'AWS'],
-      '设计': ['UI设计', 'UE设计', 'Figma', 'Sketch', 'PhotoShop', '原型设计', '交互设计'],
-      '产品': ['需求分析', '产品规划', '用户研究', '数据分析', 'Axure', '项目管理'],
-      '算法': ['机器学习', '深度学习', 'NLP', '计算机视觉', 'PyTorch', 'TensorFlow']
-    };
+class _JobListViewState extends State<JobListView> {
+  final ScrollController _scrollController = ScrollController();
+  List<Job> _jobs = [];
+  bool _isLoading = false;
+  String? _error;
 
-    final hrNames = ['王女士', '李先生', '张女士', '刘先生', '陈女士', '赵先生', '孙女士', '周先生', '吴女士', '郑先生'];
+  @override
+  void initState() {
+    super.initState();
+    _loadJobs();
+    _scrollController.addListener(_onScroll);
+  }
 
-    return List.generate(10, (index) {
-      final company = companies[index];
-      final title = titles[index];
-      String category = '';
-      if (title.contains('开发')) category = '开发';
-      else if (title.contains('设计')) category = '设计';
-      else if (title.contains('产品')) category = '产品';
-      else if (title.contains('算法')) category = '算法';
-      else category = '开发';
+  Future<void> _loadJobs() async {
+    if (_isLoading) return;
 
-      final tagPool = allTags[category] ?? allTags['开发']!;
-      final selectedTags = (tagPool.toList()..shuffle()).take(3).toList();
-
-      return Job(
-        id: 'job_${index + 1}',
-        title: title,
-        salary: salaries[index],
-        company: company['name']!,
-        companySize: company['size']!,
-        tags: selectedTags,
-        hrName: hrNames[index],
-        location: locations[index],
-        workExperience: '3-5年',
-        education: '本科及以上',
-        benefits: ['五险一金', '年终奖', '带薪年假', '加班补助'],
-        description: '负责公司核心业务系统的开发和维护工作',
-        requirements: ['本科及以上学历', '3年以上相关工作经验', '良好的团队协作能力'],
-        status: '未投递',
-        date: DateTime.now().toString().substring(0, 10)
-      );
+    setState(() {
+      _isLoading = true;
+      _error = null;
     });
+
+    try {
+      final newJobs = widget.onLoadMore();
+      setState(() {
+        _jobs.addAll(newJobs);
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _error = '加载数据失败：$e';
+        _isLoading = false;
+      });
+    }
+  }
+
+  void refreshJobList() {
+    setState(() {
+      _jobs.clear();
+    });
+    _loadJobs();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _loadJobs();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_error != null) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_error!, style: const TextStyle(color: Colors.red)),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => _loadJobs(),
+              child: const Text('重试'),
+            ),
+          ],
+        ),
+      );
+    }
+
+    if (_jobs.isEmpty && _isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return RefreshIndicator(
+      onRefresh: () async {
+        _jobs.clear();
+        await _loadJobs();
+      },
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _jobs.length + (_isLoading ? 1 : 0),
+        itemBuilder: (context, index) {
+          if (index < _jobs.length) {
+            return JobCard(job: _jobs[index]);
+          } else {
+            return const Center(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
+
