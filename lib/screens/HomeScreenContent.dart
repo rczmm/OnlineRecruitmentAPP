b53daeacc08d6d2464e4893f4071a0ae48bcbb7c
rcz <3179623450@qq.com>
2025-03-22 20:48:48 +0800

    feat(lib): 优化网络请求和错误处理
    - 添加 CancelToken以支持取消之前的网络请求
    - 实现 _cancelPreviousRequest 方法来取消之前的请求
    - 完善 DioException 异常处理，包括超时、响应错误等
    - 优化 _refreshJobs 方法，确保在下拉刷新时取消之前的请求

--- lib/screens/HomeScreenContent.dart
+++ lib/screens/HomeScreenContent.dart
@@ -17,6 +17,7 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
   final List<Job> _allJobs = [];
   String _currentType = ''; // 保存当前选中的关键词
   String _currentTag = '';
+  CancelToken? _cancelToken;
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -43,11 +44,14 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
       // 如果type或tag发生变化，重置页码
       if ((type != null && type != _currentType) ||
           (tag != null && tag != _currentTag)) {
+        _cancelPreviousRequest();
         _page = 1;
         _allJobs.clear();
         _currentType = type ?? _currentType;
         _currentTag = tag ?? _currentTag;
       }
+
+      _cancelToken = CancelToken();
 
       final response = await dio.post(
         "job/list",
@@ -58,6 +62,7 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
           "pageNum": _page,
           "pageSize": _pageSize,
         },
+        cancelToken: _cancelToken,
       );
 
       if (response.statusCode == 200 && response.data['code'] == 200) {
@@ -76,19 +81,43 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
         _showErrorDialog('请检查你的网络连接并重试。');
         return [];
       }
-    } on DioException {
-      // 处理 Dio 异常，例如网络连接错误
-      _showErrorDialog('请检查你的网络连接并重试。');
-      return [];
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.cancel) {
+        // 请求被取消，不需要处理
+        return [];
+      } else if (e.type == DioExceptionType.connectionTimeout ||
+          e.type == DioExceptionType.receiveTimeout ||
+          e.type == DioExceptionType.sendTimeout) {
+        // 处理超时异常，例如显示错误消息
+        _showErrorDialog('网络连接超时，请检查你的网络连接并重试。');
+        return [];
+      } else if (e.type == DioExceptionType.badResponse) {
+        // 处理响应错误，例如服务器返回错误码
+        _showErrorDialog('服务器错误，请稍后重试。');
+        return [];
+      } else {
+        // 处理 Dio 异常，例如网络连接错误
+        _showErrorDialog('请检查你的网络连接并重试。');
+        return [];
+      }
     } catch (e) {
       _showErrorDialog('发生错误：$e');
       return [];
     }
   }
 
+  // 添加一个方法来取消之前的请求
+  void _cancelPreviousRequest() {
+    if (_cancelToken != null && !_cancelToken!.isCancelled) {
+      _cancelToken!.cancel('用户取消了之前的请求');
+      _cancelToken = null; // 重置 CancelToken
+    }
+  }
+
   // 添加这个方法来处理下拉刷新
   Future<List<Job>> _refreshJobs(String? type, String? tag) async {
     setState(() {
+      _cancelPreviousRequest(); // 取消之前的请求
       _page = 1;
       _allJobs.clear();
       if (type != null) {
